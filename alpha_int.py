1d70b1e6fd568bc2e3bc14b17eb87dacd7e0aee0
loyolastalin <loyolastalin@gmail.com>
2020-06-11 19:11:46 +0000

    Updated for verification

--- alpha_int.py
+++ alpha_int.py
@@ -1,1 +1,8 @@
-# alpha to integerx = '92666'b = 0for item in x:    b = (b *10) + (ord(item)- ord('0'))   print(b)print(type(b))
+# alpha to integer
+x = '92666'
+b = 0
+for item in x:
+    b = (b *10) + (ord(item)- ord('0'))
+   
+print(b)
+print(type(b))
